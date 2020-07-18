e9f641ff548b75aa917d64f0db73f8435126ef70
eric-programming <eric09279@outlook.com>
2020-07-17 18:01:59 -0700

    update the code

--- JavaBasics/src/DataStructure/Graphs/GraphApp.java
+++ JavaBasics/src/DataStructure/Graphs/GraphApp.java
@@ -2,6 +2,16 @@ package DataStructure.Graphs;
 
 public class GraphApp {
     public static void main(String[] args) {
+        long[] arr = new long[] { 1, 2, 3, 4, 5, 6 };
+        implementGraph(arr);
+    }
 
+    public static void implementGraph(long[] arr) {
+        // Refer to image graph3.png
+        GraphClass<Long> g = new GraphClass<>(true);// try false
+        for (int i = 0; i < arr.length - 1; i++) {
+            g.addEdge(arr[i], arr[i + 1]);
+        }
+        g.printGraph();
     }
 }
---
+++ JavaBasics/src/DataStructure/Graphs/GraphClass.java
@@ -0,0 +1,60 @@
+package DataStructure.Graphs;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+//Vertex and Edges
+public class GraphClass<T> {
+    private Map<T, List<T>> map;
+    private boolean undirectional;
+
+    public GraphClass(boolean isUndirectional) {
+        super();
+        map = new HashMap<>();
+        undirectional = isUndirectional;
+    }
+
+    public void addVertex(T v) {
+        map.put(v, new LinkedList<T>());
+    }
+
+    public void addEdge(T start, T end) {
+        if (!hasVertex(start)) {
+            addVertex(start);
+        }
+        if (!hasVertex(end)) {
+            addVertex(end);
+        }
+        map.get(start).add(end);
+        // Undirectional means 2 ways
+        if (undirectional) {
+            map.get(end).add(start);
+        }
+    }
+
+    public int getEdgesCount() {
+        int count = 0;
+        for (T e : map.keySet()) {
+            count += map.get(e).size();
+        }
+        if (undirectional) {
+            count /= 2;
+        }
+        return count;
+    }
+
+    public boolean hasVertex(T v) {
+        return map.containsKey(v);
+    }
+
+    public boolean hasEdge(T start, T end) {
+        return map.get(start).contains(end);
+    }
+
+    public void printGraph() {
+        System.out.println(map);
+    }
+
+}
