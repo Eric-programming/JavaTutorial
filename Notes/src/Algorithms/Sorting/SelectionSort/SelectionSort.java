7119cd06c6a45be2737bf106e2235f1411a172b6
eric-programming <eric09279@outlook.com>
2020-08-14 22:27:58 -0700

    updates selectionSort

--- Notes/src/Algorithms/Sorting/SelectionSort/SelectionSort.java
+++ Notes/src/Algorithms/Sorting/SelectionSort/SelectionSort.java
@@ -5,48 +5,29 @@ import java.util.Arrays;
 // Time Complexity: O(n^2)
 public class SelectionSort {
     public static void main(String[] args) {
-        long[] items = new long[] { 3, 4, 2, 1 };
-        System.out.println(Arrays.toString(selectionSort(items)));
+        long[] items = new long[] { 3, 4, 2, 1 };// 1234
+        System.out.println(Arrays.toString(selectionSortMethod(items)));
 
-        long[] items2 = new long[] { 4, 3, 2, 1 };
-        System.out.println(Arrays.toString(selectionSort(items2)));
+        long[] items2 = new long[] { 4, 3, 2, 1 };// 1234
+        System.out.println(Arrays.toString(selectionSortMethod(items2)));
 
-        long[] items3 = new long[] { 6, 1, 9, 0, 3, 2 };
-        System.out.println(Arrays.toString(selectionSort(items3)));
+        long[] items3 = new long[] { 6, 1, 9, 0, 3, 2 };// 012369
+        System.out.println(Arrays.toString(selectionSortMethod(items3)));
 
     }
 
-    /**
-     * 
-     * Procedure SelectionSort (a)
-     * 
-     * Inputs: a (array)
-     * 
-     * Output: sorted array in ascending order
-     * 
-     * 1) for i = 0 to a.length -1
-     * 
-     * a. set min = i
-     * 
-     * b. for j = i + 1 to a.length
-     * 
-     * b1. if a[j] < a[min] then min = j
-     * 
-     * c. swap a[i] with a[min]
-     * 
-     * 2) return the sorted a.
-     */
-    public static long[] selectionSort(long[] a) {
+    public static long[] selectionSortMethod(long[] a) {
         for (int i = 0; i < a.length - 1; i++) {
-            int min = i;
+            int minIndex = i;
             for (int j = i + 1; j < a.length; j++) {
-                if (a[min] > a[j]) {
-                    min = j;
+                if (a[minIndex] > a[j]) {
+                    minIndex = j;
                 }
             }
-            long temp = a[i];
-            a[i] = a[min];
-            a[min] = temp;
+            // once we found the min index, we swap the value position
+            long tempValue = a[minIndex];
+            a[minIndex] = a[i];
+            a[i] = tempValue;
         }
         return a;
     }
